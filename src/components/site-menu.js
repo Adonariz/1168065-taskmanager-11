e032e5a281a043e27a36d10f8d43ca3ce6262d1f
A Zhila <aleksej.zhila@gmail.com>
2020-05-10 20:12:54 +0300

    Добавляет классы в остальные модули

--- src/components/site-menu.js
+++ src/components/site-menu.js
@@ -1,4 +1,6 @@
-export const createSiteMenuTemplate = () => {
+import {createElement} from "../utils";
+
+const createSiteMenuTemplate = () => {
   return (
     `<section class="control__btn-wrap">
       <input
@@ -26,3 +28,26 @@ export const createSiteMenuTemplate = () => {
     </section>`
   );
 };
+
+export default class SiteMenu {
+  constructor(task) {
+    this._task = task;
+    this._element = null;
+  }
+
+  getTemplate() {
+    return createSiteMenuTemplate(this._task);
+  }
+
+  getElement() {
+    if (!this._element) {
+      this._element = createElement(this.getTemplate());
+    }
+
+    return this._element;
+  }
+
+  removeElement() {
+    this._element = null;
+  }
+}
--- src/components/sorting.js
+++ src/components/sorting.js
@@ -1,7 +1,32 @@
-export const createSortingTemplate = () => {
+import {createElement} from "../utils";
+
+const createSortingTemplate = () => {
   return `<div class="board__filter-list">
     <a href="#" class="board__filter">SORT BY DEFAULT</a>
     <a href="#" class="board__filter">SORT BY DATE up</a>
     <a href="#" class="board__filter">SORT BY DATE down</a>
   </div>`;
 };
+
+export default class Sort {
+  constructor(task) {
+    this._task = task;
+    this._element = null;
+  }
+
+  getTemplate() {
+    return createSortingTemplate(this._task);
+  }
+
+  getElement() {
+    if (!this._element) {
+      this._element = createElement(this.getTemplate());
+    }
+
+    return this._element;
+  }
+
+  removeElement() {
+    this._element = null;
+  }
+}
