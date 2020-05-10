f7a0613e81c202c55e707016259a1ba32dbd94d4
A Zhila <aleksej.zhila@gmail.com>
2020-05-10 20:51:51 +0300

    Добавляет отрисовку хэдера и фильтра

--- src/main.js
+++ src/main.js
@@ -15,43 +15,19 @@ const TASK_COUNT = 22;
 const SHOWING_TASKS_COUNT_ON_START = 8;
 const SHOWING_TASKS_COUNT_BY_BUTTON = 8;
 
-// Рендеринг фильтров
+const renderTask = () => {
+
+};
+
+const renderBoard = () => {
+
+};
+
 const siteMainElement = document.querySelector(`.main`);
 const siteHeaderElement = siteMainElement.querySelector(`.main__control`);
+
 const filters = generateFilters();
-
-render(siteHeaderElement, createSiteMenuTemplate());
-render(siteMainElement, createFilterTemplate(filters));
-render(siteMainElement, createBoardTemplate());
-
-// Рендеринг блока сортировки
-const boardElement = siteMainElement.querySelector(`.board`);
-render(boardElement, createSortingTemplate(), `afterbegin`);
-
-// Рендеринг карточек заданий
 const tasks = generateTasks(TASK_COUNT);
 
-const taskListElement = siteMainElement.querySelector(`.board__tasks`);
-render(taskListElement, createTaskEditTemplate(tasks[0]));
-
-let showingTasksCount = SHOWING_TASKS_COUNT_ON_START;
-
-tasks.slice(1, showingTasksCount)
-  .forEach((task) => render(taskListElement, createTaskTemplate(task)));
-
-// Рендеринг кнопки LOAD MORE
-render(boardElement, createLoadMoreButtonTemplate());
-
-const loadMoreButton = boardElement.querySelector(`.load-more`);
-
-loadMoreButton.addEventListener(`click`, () => {
-  const prevTasksCount = showingTasksCount;
-  showingTasksCount = showingTasksCount + SHOWING_TASKS_COUNT_BY_BUTTON;
-
-  tasks.slice(prevTasksCount, showingTasksCount)
-    .forEach((task) => render(taskListElement, createTaskTemplate(task)));
-
-  if (showingTasksCount >= tasks.length) {
-    loadMoreButton.remove();
-  }
-});
+render(siteHeaderElement, new SiteMenuComponent().getElement(), RenderPosition.BEFOREEND);
+render(siteMainElement, new FilterComponent(filters).getElement(), RenderPosition.BEFOREEND);
