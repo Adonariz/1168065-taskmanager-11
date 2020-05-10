74b15f69494c23c107b1389bbbc7540452274f9e
A Zhila <aleksej.zhila@gmail.com>
2020-05-10 21:36:07 +0300

    Добавляет отрисовку компонентов

--- src/main.js
+++ src/main.js
@@ -15,6 +15,9 @@ const TASK_COUNT = 22;
 const SHOWING_TASKS_COUNT_ON_START = 8;
 const SHOWING_TASKS_COUNT_BY_BUTTON = 8;
 
+const siteMainElement = document.querySelector(`.main`);
+const siteHeaderElement = siteMainElement.querySelector(`.main__control`);
+
 const renderTask = (taskListElement, task) => {
   const taskComponent = new TaskComponent(task);
   const taskEditComponent = new TaskEditComponent(task);
@@ -38,6 +41,9 @@ const renderTask = (taskListElement, task) => {
 };
 
 const renderBoard = (boardComponent, tasks) => {
+  render(boardComponent.getElement(), new SortComponent().getElement(), RenderPosition.BEFOREEND);
+  render(boardComponent.getElement(), new TasksComponent().getElement(), RenderPosition.BEFOREEND);
+
   const taskListElement = boardComponent.getElement().querySelector(`.board__tasks`);
   const loadMoreButtonComponent = new LoadMoreButtonComponent();
 
@@ -59,18 +65,19 @@ const renderBoard = (boardComponent, tasks) => {
     }
   };
 
-  render(boardComponent.getElement(), new SortComponent().getElement(), RenderPosition.BEFOREEND);
-  render(boardComponent.getElement(), new TasksComponent().getElement(), RenderPosition.BEFOREEND);
   render(boardComponent.getElement(), loadMoreButtonComponent.getElement(), RenderPosition.BEFOREEND);
 
   loadMoreButtonComponent.getElement().addEventListener(`click`, onLoadMoreButtonClick());
 };
 
-const siteMainElement = document.querySelector(`.main`);
-const siteHeaderElement = siteMainElement.querySelector(`.main__control`);
-
 const filters = generateFilters();
 const tasks = generateTasks(TASK_COUNT);
 
-render(siteHeaderElement, new SiteMenuComponent().getElement(), RenderPosition.BEFOREEND);
-render(siteMainElement, new FilterComponent(filters).getElement(), RenderPosition.BEFOREEND);
+const boardComponent = new BoardComponent();
+const siteMenuComponent = new SiteMenuComponent();
+const filterComponent = new FilterComponent(filters);
+
+render(siteHeaderElement, siteMenuComponent.getElement(), RenderPosition.BEFOREEND);
+render(siteMainElement, filterComponent.getElement(), RenderPosition.BEFOREEND);
+render(siteMainElement, boardComponent.getElement(), RenderPosition.BEFOREEND);
+renderBoard(boardComponent, tasks);
