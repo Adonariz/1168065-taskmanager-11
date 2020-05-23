df5b845e004ec17c4d95b69bcaf1b384ad919a55
A Zhila <aleksej.zhila@gmail.com>
2020-05-23 19:09:55 +0300

    Добавляет moment

--- src/components/task-edit.js
+++ src/components/task-edit.js
@@ -1,5 +1,5 @@
-import {COLORS, DAYS, MONTH_NAMES} from "../const";
-import {formatTime} from "../utils/common";
+import {COLORS, DAYS} from "../const";
+import {formatDate, formatTime} from "../utils/common";
 import AbstractSmartComponent from "./abstract-smart-component";
 import flatpickr from "flatpickr";
 
@@ -60,7 +60,7 @@ const createTaskEditTemplate = (task, options = {}) => {
   const isBlockSaveButton = (isDateShowing && isRepeatingTask) ||
     (isRepeatingTask && !isRepeating(activeRepeatingDays));
 
-  const date = (isDateShowing && dueDate) ? `${dueDate.getDate()} ${MONTH_NAMES[dueDate.getMonth()]}` : ``;
+  const date = (isDateShowing && dueDate) ? formatDate(dueDate) : ``;
   const time = (isDateShowing && dueDate) ? formatTime(dueDate) : ``;
 
   const repeatClass = isRepeatingTask ? `card--repeat` : ``;
