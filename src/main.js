b3803a2f76f32f9e8ba6c07a10fb38a921a8a326
A Zhila <aleksej.zhila@gmail.com>
2020-05-26 00:24:05 +0300

    Подключает модель в контроллер board.js

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import BoardComponent from "./components/board.js";
 import BoardController from "./controllers/board";
 import FilterComponent from "./components/filter.js";
 import SiteMenuComponent from "./components/site-menu.js";
+import TasksModel from "./models/tasks";
 import {generateTasks} from "./mock/task.js";
 import {generateFilters} from "./mock/filter.js";
 import {render, RenderPosition} from "./utils/render";
@@ -13,11 +14,13 @@ const siteHeaderElement = siteMainElement.querySelector(`.main__control`);
 
 const filters = generateFilters();
 const tasks = generateTasks(TASK_COUNT);
+const tasksModel = new TasksModel();
+tasksModel.setTasks(tasks);
 
 const siteMenuComponent = new SiteMenuComponent();
 const filterComponent = new FilterComponent(filters);
 const boardComponent = new BoardComponent();
-const boardController = new BoardController(boardComponent);
+const boardController = new BoardController(boardComponent, tasksModel);
 
 render(siteHeaderElement, siteMenuComponent, RenderPosition.BEFOREEND);
 render(siteMainElement, filterComponent, RenderPosition.BEFOREEND);
